fd675604312f602261a0c8ce366890e43c52d5cc
Georgios Konstantopoulos <me@gakonst.com>
2021-09-04 19:16:53 +0300

    try to get ffi working for metadata parsing

--- src/test/utils/Hevm.sol
+++ src/test/utils/Hevm.sol
@@ -8,4 +8,5 @@ abstract contract Hevm {
     function roll(uint x) public virtual;
     // sets the slot loc of contract c to val
     function store(address c, bytes32 loc, bytes32 val) public virtual;
+    function ffi(string[] calldata) external virtual returns (bytes memory);
 }
