763fa8ba0cfe34d8fcf5325a26408840b63c6ed2
Georgios Konstantopoulos <me@gakonst.com>
2021-09-06 04:40:17 +0300

    refactor: move deploy helper to common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -5,6 +5,23 @@ export OUT_DIR=${OUT_DIR:-$PWD/out}
 ADDRESSES_FILE="$OUT_DIR/addresses.json"
 export CONFIG_FILE="${OUT_DIR}/config.json"
 
+deploy() {
+    NAME=$1
+    SIG=$2
+    ARGS=${@:3}
+
+    # get the bytecode from the compiled file
+    PATTERN=".contracts[\"src/$NAME.sol\"].$NAME.evm.bytecode.object"
+    BYTECODE=0x$(cat out/dapp.sol.json | jq -r "$PATTERN")
+
+    # estimate gas
+    GAS=$(seth estimate --create $BYTECODE $SIG $ARGS --rpc-url $RPC_URL --from $FROM)
+
+    # deploy
+    ADDRESS=$(dapp create $NAME $ARGS -- --rpc-url $RPC_URL --from $FROM --gas $GAS)
+    echo $ADDRESS
+}
+
 # loads addresses as key-value pairs from $ADDRESSES_FILE and exports them as
 # environment variables.
 loadAddresses() {
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,23 +10,6 @@ MAINNET_LOOT=0xff9c1b15b16263c61d017ee9f65c50e4ae0113d7
 # Default to it if nothing is provided
 LOOT="${LOOT:-$MAINNET_LOOT}"
 
-deploy() {
-    NAME=$1
-    SIG=$2
-    ARGS=${@:3}
-
-    # get the bytecode from the compiled file
-    PATTERN=".contracts[\"src/$NAME.sol\"].$NAME.evm.bytecode.object"
-    BYTECODE=0x$(cat out/dapp.sol.json | jq -r "$PATTERN")
-
-    # estimate gas
-    GAS=$(seth estimate --create $BYTECODE $SIG $ARGS --rpc-url $RPC_URL --from $FROM)
-
-    # deploy
-    ADDRESS=$(dapp create $NAME $ARGS -- --rpc-url $RPC_URL --from $FROM --gas $GAS)
-    echo $ADDRESS
-}
-
 # Deploy.
 LootLooseAddr=$(deploy LootLoose 'LootLoose(address)' $LOOT)
 log "LootLoose deployed at:" $LootLooseAddr
